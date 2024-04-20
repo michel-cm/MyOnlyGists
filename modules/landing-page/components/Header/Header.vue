f7a1839f23bfdbf6c0c52ededfa65180f9507fd5
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2024-04-20 15:26:56 -0300

    feat: Logo/Header

--- modules/landing-page/components/Header/Header.vue
+++ modules/landing-page/components/Header/Header.vue
@@ -1,11 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Logo from '@/components/Logo/Logo.vue'
+
+</script>
 
 <template>
   <header class="w-full shadow">
     <nav class="bg-white px-4 lg:px-6 py-2">
       <div class="flex justify-center md:justify-between items-center mx-auto max-w-screen-xl">
         <div class="flex items-center">
-          logo
+          <nuxt-link to="/">
+            <Logo />
+          </nuxt-link>
         </div>
         
         <div class="hiddern md:flex items-center">
--- modules/landing-page/screens/Home/Home.vue
+++ modules/landing-page/screens/Home/Home.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
 import Header from '@/modules/landing-page/components/Header/Header.vue'
 import Hero from '@/modules/landing-page/components/Hero/Hero.vue'
+import MainContent from '@/components/MainContent/MainContent.vue'
+
 </script>
 
 <template>
-    <Header />
-    <Hero />
+  <MainContent>
+    <template #header>   
+      <Header />
+    </template>
+    <template #content>   
+      <Hero />
+    </template>
+  </MainContent>
 </template>
