f39469c0357422934a0124e2c893d807b61e3433
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2024-04-20 13:48:56 -0300

    base

---
+++ modules/landing-page/components/Header/Header.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="w-full shadow">
+    <nav class="bg-white px-4 lg:px-6 py-2">
+      <div class="flex justify-center md:justify-between items-center mx-auto max-w-screen-xl">
+        <div class="flex items-center">
+          logo
+        </div>
+        
+        <div class="hiddern md:flex items-center">
+          <Button
+            label="Continuar como creator"
+            icon-pos="right"
+            icon="pi pi-arrow-right"
+          />
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
