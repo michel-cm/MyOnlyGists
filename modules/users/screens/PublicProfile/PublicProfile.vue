dd1ec2932cd043194e0f6f2a67e7b8f58f33c484
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2024-04-22 06:22:31 -0300

    feat: modulo de reports

--- modules/users/screens/PublicProfile/PublicProfile.vue
+++ modules/users/screens/PublicProfile/PublicProfile.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts">
 import PublicHeadline from '../components/PublicHeadline/PublicHeadline.vue';
+import WidgetGroup from '@/modules/reports/components/Widget/Group/Group.vue';
+import WidgetCondensed from '@/modules/reports/components/Widget/Condensed/Condensed.vue';
+
 </script>
 
 <template>
   <PublicHeadline />
+
+  <WidgetGroup>
+    <WidgetCondensed :value="10" label="Gists do total" />
+    <WidgetCondensed :value="5" label="Gists gratuitos" />
+    <WidgetCondensed :value="5" label="Gists pagos"/>
+  </WidgetGroup>
 </template>
