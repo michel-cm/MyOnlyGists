f70f9a38f800106e477b465c9e48936a4bb5c662
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2024-04-22 09:23:04 -0300

    feat: HoC loader para WidgetGroup

--- modules/users/screens/PublicProfile/PublicProfile.vue
+++ modules/users/screens/PublicProfile/PublicProfile.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import PublicHeadline from '../components/PublicHeadline/PublicHeadline.vue';
 import WidgetGroup from '@/modules/reports/components/Widget/Group/Group.vue';
+import WidgetGroupLoader from '@/modules/reports/components/Widget/Group/loader.vue';
 import WidgetCondensed from '@/modules/reports/components/Widget/Condensed/Condensed.vue';
 
 </script>
@@ -9,8 +10,10 @@ import WidgetCondensed from '@/modules/reports/components/Widget/Condensed/Conde
   <PublicHeadline />
 
   <WidgetGroup>
-    <WidgetCondensed :value="10" label="Gists do total" />
-    <WidgetCondensed :value="5" label="Gists gratuitos" />
-    <WidgetCondensed :value="5" label="Gists pagos"/>
+    <WidgetGroupLoader :loading="true" :amount="3">
+      <WidgetCondensed :value="10" label="Gists do total" />
+      <WidgetCondensed :value="5" label="Gists gratuitos" />
+      <WidgetCondensed :value="5" label="Gists pagos"/>
+    </WidgetGroupLoader>
   </WidgetGroup>
 </template>
